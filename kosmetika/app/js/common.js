bbacc11401ada055fee76632d6386e40d475a3fd
bipeo <katamason1990@gmail.com>
2016-03-02 11:34:58 +0200

    Merge origin/master
    Conflicts:
    	gosp/app/service-item.html
    	gosp/app/style.css

--- kosmetika/app/js/common.js
+++ kosmetika/app/js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	$("select").selecter();
+	
+	$('.program_answer').css({'display':'none'});
+
+	$('.program_item').click(function(){
+		$('.program_item').not($(this)).removeClass('open');
+		$('.program_answer').not($(this).next('.program_answer')).slideUp(500);
+		$(this).next('.program_answer').slideToggle(500);
+		$(this).toggleClass('open');
+	});
 });
