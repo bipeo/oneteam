06db8be054c3fe308eb519f63744f300e8aec9c4
bipeo <katamason1990@gmail.com>
2016-03-18 13:05:20 +0200

    Merge origin/master
    Conflicts:
    	gosp/app/service-item.html
    	gosp/app/style.css

--- med/app/js/common.js
+++ med/app/js/common.js
@@ -4,8 +4,26 @@ $(document).ready(function () {
 		 dots: true
 	 });
 	$('#slider_2').slick({
-		 arrows: true,
-		 dots: false,
-                 fade: true,
+		arrows: true,
+		dots: false,
+		fade: true,
+		infinite: false
 	 });
+	$('#slider_3, #slider_4').slick({
+		arrows: true,
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		dots: false
+	 });
+	
+	// табы 
+    $('.link_1 a').click(function(e) {
+        e.preventDefault();
+        $('.link_1 .active').removeClass('active');
+        $(this).addClass('active');
+        var tab = $(this).attr('href');
+        $('.tabs').not(tab).css({'visibility':'hidden'});
+        $(tab).css({'visibility':'visible'});
+    });
 });
