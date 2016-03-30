26aace6b4dba03ec0c02359717d9def396255c14
bipeo <katamason1990@gmail.com>
2016-03-30 21:00:42 +0300

    Merge origin/master
    Conflicts:
    	izmail/app/js/common.js

--- baltica/app/js/common.js
+++ baltica/app/js/common.js
@@ -5,3 +5,42 @@
           
            $('body').css('min-height',minh + addmar +'px'); 
         });
+        
+        
+        
+        $(document).ready(function()
+        {
+
+            $(window).load(function()
+                {
+                    getWidthAndHeight()
+                }
+            );
+
+
+            $(window).resize(function()
+                {
+                    getWidthAndHeight()
+                }
+            );
+
+
+            function getWidthAndHeight() {
+                //var ass = $('.ass_box').show();
+                var scrollHeight = document.documentElement.scrollHeight,
+                clientHeight = document.documentElement.clientHeight;
+                scrollHeight = Math.max(scrollHeight, clientHeight);
+
+                    var wpbar = $('.cabtop').outerHeight();
+                   scrollHeight -=   wpbar;
+                $(".opsidebar").css("min-height", scrollHeight + "px");
+                
+            }
+            
+            
+            
+          
+            
+            
+        }
+    );
