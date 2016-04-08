f7b94aa6a6aa3174227fe3c915f91bb5e6a980fb
bipeo <katamason1990@gmail.com>
2016-04-08 20:49:00 +0300

    Merge origin/master
    Conflicts:
    	baltica/app/js/common.js

--- baltica/app/js/common.js
+++ baltica/app/js/common.js
@@ -8,6 +8,30 @@ $(document).ready(function()
 //		$('#menu_mobile').css('height', heightMenu );
 	});
 	
+	/*detect browser*/	
+    var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+    var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+    var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+    var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+    if ((is_chrome)&&(is_safari)) {is_safari=false;}
+    if ((is_chrome)&&(is_opera)) {is_chrome=false;}
+   /* end detect browser*/	
+
+    
+    if(is_opera) {
+        $('body').addClass('is_opera');
+    }
+    if(is_chrome) {
+        $('body').addClass('is_chrome');
+    }
+    if(is_firefox) {
+        $('body').addClass('is_firefox');
+    }
+    if(is_safari) {
+        $('body').addClass('is_safari');
+    }
+	
 		///////////////////////////////////////////////////////////////////////// menu_mobile
 
 		var pull 		= $('.btn_menu');
