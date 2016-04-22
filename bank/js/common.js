bdf893813de9148a077b557af656a943ce4e75c9
bipeo <katamason1990@gmail.com>
2016-04-22 23:28:47 +0300

    Merge origin/master
    Conflicts:
    	baltica/app/style.css

--- bank/js/common.js
+++ bank/js/common.js
@@ -68,3 +68,32 @@ jQuery(window).scroll(function(){
         jQuery('body').removeClass('bg_2');
     }
 });
+
+/****  focus box  in-view ****/
+var $animation_elements = $('.bussines > li, .block_3_ul_2 > li, .block_4_ul > li');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+/**** focus box  in-view end ***/
