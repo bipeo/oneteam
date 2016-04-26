47c7e827c1b503d109f38f0dea02b57a6a13bc82
bipeo <katamason1990@gmail.com>
2016-04-26 15:40:05 +0300

    Merge origin/master
    Conflicts:
    	baltica/app/style.css

--- bank/js/common.js
+++ bank/js/common.js
@@ -126,12 +126,20 @@ function check_if_in_view() {
     var element_bottom_position = (element_top_position + element_height);
  
     //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
+   /* if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
       $element.addClass('in-view');
     } else {
       $element.removeClass('in-view');
+    } */
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+     // $element.removeClass('in-view');
     }
+    
+    
   });
 }
 
