b56569ba17f8801f78f9396a90e4c4343b981e5b
bipeo <katamason1990@gmail.com>
2016-06-01 11:40:51 +0300

    Merge origin/master
    Conflicts:
    	sport/css/style.css

---
+++ sport/js/dpickercust.js
@@ -0,0 +1,58 @@
+  function showdatepicker() {
+          $( "#from, #to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1,
+            dateFormat : 'dd.mm',
+            nextText: '',
+            prevText: '',
+            onSelect: function( selectedDate ) {
+                if(this.id == 'from'){
+                  var dateMin = $('#from').datepicker("getDate");
+                  var rMin = new Date(dateMin.getFullYear(), dateMin.getMonth(),dateMin.getDate() + 1); 
+                  var rMax = new Date(dateMin.getFullYear(), dateMin.getMonth(),dateMin.getDate() + 31); 
+                  $('#from').datepicker("option","minDate",rMin);
+                  $('#to').datepicker("option","maxDate",rMax);                    
+                }
+                
+            },
+            beforeShow: function(input, inst) {
+       $('#ui-datepicker-div').removeClass(function() {
+           return $('input').get(0).id; 
+       });
+       $('#ui-datepicker-div').addClass('custdper');
+        }
+        });
+    } 
+    
+
+        function populateDefaultValues() {
+            var today = new Date();
+            var month = today.getMonth() - 1,
+                year = today.getFullYear();
+            if (month < 0) {
+                month = 11;
+                year -= 1;
+            }
+            var oneMonthAgo = new Date(year, month, today.getDate());
+
+            $('#to').val($.datepicker.formatDate('dd.mm', today));
+            $('#from').val($.datepicker.formatDate('dd.mm', today));
+        }
+
+        $(function() {
+            populateDefaultValues();
+            showdatepicker();
+        });
+/*
+ * 
+ * 
+ *  <div class="demo"> 
+
+                <label for="from">From</label> 
+                <input type="text" id="from" name="from"/> 
+                <label for="to">to</label> 
+                <input type="text" id="to" name="to"/> 
+
+                </div>
+ *****/
