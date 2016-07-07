7b2a17e1634e46a2d7817ab4f5caaf551e79aa71
bipeo <katamason1990@gmail.com>
2016-07-07 12:29:09 +0300

    Merge origin/master
    Conflicts:
    	repo_locks/css/style.css

--- viktoria/app/js/common.js
+++ viktoria/app/js/common.js
@@ -4,4 +4,27 @@ $(document).ready(function(){
         nav: false,
         loop: true
     });
+    
+    $("#date_from").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+//        showOn: "button",        
+//        showAnim: "slideDown",
+        dateFormat: 'd MM yy'
+        onClose: function(selectedDate) {
+            $("#date_to").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#date_to").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+//        showOn: "button",        
+        showAnim: "slideDown",
+        dateFormat: 'd MM yy'
+        onClose: function(selectedDate) {
+            $("#date_from").datepicker("option", "maxDate", selectedDate);
+        }
+    });
 });
