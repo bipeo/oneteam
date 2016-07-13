77a2f36b07ed910429d7c9803050020a34da479c
bipeo <katamason1990@gmail.com>
2016-07-13 16:14:25 +0300

    Merge origin/master
    Conflicts:
    	repo_locks/css/style.css

---
+++ cats/app/js/common.js
@@ -0,0 +1,10 @@
+$('#circle').circleProgress({
+    value: 0.35,
+    size: 275,
+    fill: {
+        //gradient: ["red", "orange"]
+        color: "#a71850"
+    },
+    emptyFill: "#a87880",
+    thickness: 9
+});
