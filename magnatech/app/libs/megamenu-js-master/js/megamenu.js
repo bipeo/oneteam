53de54f80a498b5dced4cce5dce625e954ce69bd
bipeo <katamason1990@gmail.com>
2016-08-02 16:40:13 +0300

    Merge origin/master
    Conflicts:
    	magnatech/app/js/common.js
    	magnatech/app/main.html
    	magnatech/app/style.css

--- magnatech/app/libs/megamenu-js-master/js/megamenu.js
+++ magnatech/app/libs/megamenu-js-master/js/megamenu.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
     //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
 
-    $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\">Navigation</a>");
+    $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\"></a>");
 
     //Adds menu-mobile class (for mobile toggle menu) before the normal menu
     //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
@@ -18,7 +18,7 @@ $(document).ready(function () {
     //Done this way so it can be used with wordpress without any trouble
 
     $(".menu > ul > li").hover(function (e) {
-        if ($(window).width() > 943) {
+        if ($(window).width() > 640) {
             $(this).children("ul").stop(true, false).fadeToggle(150);
             e.preventDefault();
         }
@@ -26,7 +26,7 @@ $(document).ready(function () {
     //If width is more than 943px dropdowns are displayed on hover
 
     $(".menu > ul > li").click(function () {
-        if ($(window).width() <= 943) {
+        if ($(window).width() <= 640) {
             $(this).children("ul").fadeToggle(150);
         }
     });
